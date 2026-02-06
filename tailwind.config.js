6f491f4e04dec3257d42062061883dc931604ea0
Matheus kiev <93486873+Matheuskievao@users.noreply.github.com>
2026-02-05 20:27:56 -0400

    organizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 tailwind.config = 
    {
       // Note: config only includes the used styles & variables of your selection
-      content: ["./src/**/*.{html,vue,svelte,js,ts,jsx,tsx}"],
+      content: ["/src/**/*.{html,vue,svelte,js,ts,jsx,tsx}"],
       theme: {
         extend: {
           fontFamily: {
